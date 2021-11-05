e7c0afacae4c7128cfb097298d540deea74be778
quan-nguyen-thuc06 <quannguyenthuc06@gmail.com>
2021-11-05 19:34:35 +0700

    update App.js

--- pos/src/App.js
+++ pos/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Reports from './pages/Reports';
@@ -9,7 +8,7 @@ import Header from './components/Header';
 // import Datepick from './components/Datepick'
 // import Sidebar from './components/topbar/Topbar'
 // import BasicDateRangePicker from './components/Datepick'
-import Login from './components/Login'
+// import Login from './components/Login'
 function App() {
   return (
     <>
