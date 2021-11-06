110f05ed7d8aa0c105cbd0fec58ba1041ea616bd
TienLe <74912575+Ti4nL4@users.noreply.github.com>
2021-11-06 23:09:48 +0700

    Add files via upload

--- pos/src/App.js
+++ pos/src/App.js
@@ -4,23 +4,23 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Reports from './pages/Reports';
 import ReportProduct from './pages/ReportProduct'
-import Header from './components/Header';
+//import Header from './components/Header';
 // import Datepick from './components/Datepick'
 // import Sidebar from './components/topbar/Topbar'
 // import BasicDateRangePicker from './components/Datepick'
-// import Login from './components/Login'
+ import Login from './components/Login'
 function App() {
   return (
     <>
-     
+      
       <Router>
       
-      <Header/>
-
+      {/* <Header/> */}
         <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/reports' component={Reports} />
-          <Route path='/reportProduct' component={ReportProduct} />
+          <Route path='/' exact component={Login}/>
+          <Route path='/home' exact component={Home} />
+          <Route path='/reports' exact component={Reports} />
+          <Route path='/reportProduct'exact component={ReportProduct} />
         </Switch>
       </Router>
     </>
