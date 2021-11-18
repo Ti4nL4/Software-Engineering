89491d44aff459c001a7cd1879d8ddb3eba83cde
Bùi Hữu Tiến Đạt <dat.bui197404648@hcmut.edu.vn>
2021-11-18 15:26:09 +0700

    Add logic

--- pos/src/Component/Menu/FilterFoodBar/Item.js
+++ pos/src/Component/Menu/FilterFoodBar/Item.js
@@ -3,7 +3,7 @@ import './Item.css';
 const Item = ({item, changeFilterValue}) => {
 
 
-  return <div className='col-12 p-3' onClick={() => changeFilterValue(item.type)}>
+  return <div className='col-12 p-3' onClick={() => changeFilterValue(item.Product_Type)}>
     <div className='d-flex flex-column item col-12 p-2'>
       <img src={item.Image}
         className='circle'
